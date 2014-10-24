9a871dda9051df28a1ab1644bf796b27ffe8607b
Logan Cox <lmc@ou.edu>
2014-10-24 13:35:33 -0500

    cleaning up formatting and adding a few comments

--- ouprimo.js
+++ ouprimo.js
@@ -1,5 +1,10 @@
+
+/**
+ *  primo form submit, required for some reason
+ *
+ */
 function searchPrimo()
 {
-document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value;
-document.forms["searchForm"].submit();
+    document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value;
+    document.forms["searchForm"].submit();
 }
