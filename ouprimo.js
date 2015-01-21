4799d83e1bb0c1138f12554050f406718615e105
camp5456 <tcamp@ou.edu>
2015-01-21 16:26:21 -0600

    Revert "jQuery tweaks to make things work with multiple forms"

--- ouprimo.js
+++ ouprimo.js
@@ -3,16 +3,8 @@
  *  primo form submit, required for some reason
  *
  */
-
-(function ($) {
-  Drupal.behaviors.oublocks = {
-    attach: function (context, settings) {
-      console.log("Attaching!");
-      $(context, 'form.searchPrimo').submit(function(event) {
-        var input = $(event.target).find(":text").first();
-        input.val("any,contains," + input.val());
-        return true;
-      });
-    }
-  };
-}(jQuery));
+function searchPrimo()
+{
+    document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value;
+    document.forms["searchForm"].submit();
+}
